122ef417d4ec60568f507ffeeccd66c05f8bfbe3
olegug <68272546+olegug@users.noreply.github.com>
2020-07-21 14:45:15 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    counter = 1
+})
+input.onButtonPressed(Button.B, function () {
+    while (counter > 0) {
+        counter = -1
+        basic.pause(1000)
+    }
+})
+let counter = 0
+counter = 3
+basic.forever(function () {
+    basic.showNumber(counter)
+})
